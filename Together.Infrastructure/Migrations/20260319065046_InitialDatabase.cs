c90e0951d151446daf806171fa7bd330a66e383d
Rex Huang <im@rex.tw>
2026-03-19 14:51:56 +0800

    cd C:\REX\github\lab_Together\Together.Infrastructure
    C:\REX\github\lab_Together\Together.Infrastructure>dotnet ef migrations add InitialDatabase
    Build started...
    Build succeeded.
    Done. To undo this action, use 'ef migrations remove'
    
    C:\REX\github\lab_Together\Together.Infrastructure>dotnet ef database update
    Build started...
    Build succeeded.
    Acquiring an exclusive lock for migration application. See https://aka.ms/efcore-docs-migrations-lock for more information if this takes too long.
    Applying migration '20260319065046_InitialDatabase'.
    Done.
    
    C:\REX\github\lab_Together\Together.Infrastructure>cd C:\Users\Admin\AppData\Local
    
    C:\Users\Admin\AppData\Local>dir uddd*
     磁碟區 C 中的磁碟是 PYDEV
     磁碟區序號:  40C3-D41A
    
     C:\Users\Admin\AppData\Local 的目錄
    
    2026/03/19  下午 02:50            24,576 uddd.db
                   1 個檔案          24,576 位元組
                   0 個目錄  41,859,506,176 位元組可用

---
+++ Together.Infrastructure/Migrations/20260319065046_InitialDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Together.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Coordinator = table.Column<string>(type: "TEXT", nullable: true),
+                    Place = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
