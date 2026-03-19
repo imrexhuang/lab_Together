a221fad37188202abc9d7032ed541ea70a27da84
Rex Huang <im@rex.tw>
2026-03-19 10:32:15 +0800

    Application.Services增加EventAddResult

---
+++ Together.Application/Services/EventAddResult.cs
@@ -0,0 +1,10 @@
+namespace Together.Application.Services;
+public record EventAddResult(
+    Guid Id,
+    string name,
+    string coordinator,
+    string place,
+    float lat,
+    float lng,
+    int fee
+);
