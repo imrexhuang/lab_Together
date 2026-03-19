6097a4c9613aeea2a37c6cbfbd1f46acad379ca8
Rex Huang <im@rex.tw>
2026-03-19 11:58:23 +0800

    重構寫法,落實clean arch

--- Together.Api/Program.cs
+++ Together.Api/Program.cs
@@ -1,11 +1,14 @@
 using Together.Application.Services;
+using Together.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     builder.Services.AddControllers();
     //注入物件(沒注入測試會有System.InvalidOperationException: Unable to resolve service for type)
-    builder.Services.AddScoped<IEventService, EventService>();
+    //builder.Services.AddScoped<IEventService, EventService>();
+    //重構成以下寫法,落實clean arch
+    builder.Services.AddApplication();
 }
 
 var app = builder.Build();
