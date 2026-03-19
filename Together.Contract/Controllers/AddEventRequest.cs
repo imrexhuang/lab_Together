77fd08bd72e16764e6c71948b6ee7f0be2b87d38
Rex Huang <im@rex.tw>
2026-03-19 09:24:47 +0800

    Together.Contract增加Value Object

---
+++ Together.Contract/Controllers/AddEventRequest.cs
@@ -0,0 +1,9 @@
+namespace Together.Contract.Controller;
+public record AddEventRequest (
+    string name,
+    string coordinator,
+    string place,
+    float lat,
+    float lng,
+    int  fee
+);
