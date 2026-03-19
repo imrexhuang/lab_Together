d75fec563fc780ba7d2d105c9175efd869c03b57
Rex Huang <im@rex.tw>
2026-03-19 15:43:31 +0800

    ADD EventController

---
+++ Together.Web/Controllers/EventController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Together.Web.Controllers;
+public class EventController:Controller
+{
+    public String Index()
+    {
+        return "clean arch 要連結到MVC";
+    }
+    
+}
