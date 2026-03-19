a0d6fb14fde643992aa9cd10f3ca76b2164468ab
Rex Huang <im@rex.tw>
2026-03-19 14:06:13 +0800

    EventService使用IEventRepository
      Together.Application net10.0 成功但有 3 個警告 (0.3 秒) → Together.Application\bin\Debug\net10.0\Together.Application.dll
        C:\REX\github\lab_Together\Together.Application\Services\EventService.cs(29,39): warning CS8604: 'EventQueryResult.EventQueryResult(string name, string coordinator, string place, float lat, float lng, int fee)' 中的參數 'name' 可能有 Null 參考引數。
        C:\REX\github\lab_Together\Together.Application\Services\EventService.cs(29,47): warning CS8604: 'EventQueryResult.EventQueryResult(string name, string coordinator, string place, float lat, float lng, int fee)' 中的參數 'coordinator' 可能有 Null 參考引數。
        C:\REX\github\lab_Together\Together.Application\Services\EventService.cs(29,62): warning CS8604: 'EventQueryResult.EventQueryResult(string name, string coordinator, string place, float lat, float lng, int fee)' 中的參數 'place' 可能有 Null 參考引數。

--- Together.Application/Services/EventService.cs
+++ Together.Application/Services/EventService.cs
@@ -1,16 +1,36 @@
+using Together.Application.Common.Interfaces;
+using Together.Domain.Entity;
+
 namespace Together.Application.Services;
 
-public class EventService : IEventService
+public class EventService(IEventRepository repository) : IEventService
 {
+    private readonly IEventRepository repository = repository;
     public EventAddResult add(string name, string coordinator, string place, float lat, float lng, int fee)
     {
-        return new EventAddResult(Guid.NewGuid(), name, coordinator,place, lat, lng, fee);
+        Event e = new()
+        {
+            Name = name,
+            Coordinator = coordinator,
+            Place = place,
+            Lat = lat,
+            Lng = lng,
+            Fee = fee
+        };
+        repository.AddEvent(e);
+        return new EventAddResult(Guid.NewGuid(), name, coordinator, place, lat, lng, fee);
     }
 
     public EventQueryResult[] query(float lat, float lng, float len)
     {
-        EventQueryResult result = new("name","coor","tpe",4.0f,5.0f,600);
-        EventQueryResult[] results = [result, result];
-        return results;
+        List<EventQueryResult> results = [];
+        foreach(Event r in repository.GetAll())
+        {
+        EventQueryResult result = new(r.Name, r.Coordinator, r.Place, r.Lat, r.Lng, r.Fee);    
+        results.Add(result);
+        }
+                
+
+        return [.. results];
     }
 }
