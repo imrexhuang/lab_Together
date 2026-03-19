0804b4e24718b563cc2f534a0cebf007b37000ff
Rex Huang <im@rex.tw>
2026-03-19 10:18:31 +0800

    實作EventController 再 dotnet run --project .\Together.Api\

--- Together.Api/Controllers/EventController.cs
+++ Together.Api/Controllers/EventController.cs
@@ -5,16 +5,20 @@ namespace Together.Api.Controllers;
 
 [ApiController]
 [Route("api")]
-public class EventController:ControllerBase
+public class EventController : ControllerBase
 {
     [HttpPost("add-event")]
     public IActionResult AddEvent(AddEventRequest request)
     {
-        return Ok(request);
+        AddEventResponse response = new(Guid.NewGuid(), request.name,
+        request.coordinator, request.place, request.lat, request.lng, request.fee);
+        return Ok(response);
     }
     [HttpPost("query-event")]
     public IActionResult QueryEvent(QueryEventRequest request)
     {
-        return Ok(request);
+        QueryEventResponse response = new("name1","coordinator2","place3",4.0f,5.0f,6);
+        QueryEventResponse[] events = [response, response, response];
+        return Ok(events);
     }
 }
