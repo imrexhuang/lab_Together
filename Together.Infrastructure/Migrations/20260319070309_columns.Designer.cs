13125b03c63a05cf6a2491b01899b4c111329996
Rex Huang <im@rex.tw>
2026-03-19 15:04:10 +0800

    cd C:\REX\github\lab_Together\Together.Infrastructure
    C:\REX\github\lab_Together\Together.Infrastructure>dotnet ef migrations add columns
    Build started...
    Build succeeded.
    Done. To undo this action, use 'ef migrations remove'
    
    C:\REX\github\lab_Together\Together.Infrastructure>dotnet ef database update
    Build started...
    Build succeeded.
    Acquiring an exclusive lock for migration application. See https://aka.ms/efcore-docs-migrations-lock for more information if this takes too long.
    Applying migration '20260319070309_columns'.
    Done.

--- Together.Infrastructure/Migrations/20260319070309_columns.Designer.cs
+++ Together.Infrastructure/Migrations/20260319070309_columns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Together.Infrastructure;
 
@@ -10,9 +11,11 @@ using Together.Infrastructure;
 namespace Together.Infrastructure.Migrations
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20260319070309_columns")]
+    partial class columns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
---
+++ Together.Infrastructure/Migrations/20260319070309_columns.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Together.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class columns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Fee",
+                table: "Events",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Lat",
+                table: "Events",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Lng",
+                table: "Events",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fee",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Lng",
+                table: "Events");
+        }
+    }
+}
