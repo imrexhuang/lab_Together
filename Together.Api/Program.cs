38f0e9345419470e08b549a5e025b90ab17235d0
Rex Huang <im@rex.tw>
2026-03-19 11:26:25 +0800

    Together.Api的Program.cs增加builder.Services.AddScoped<IEventService, EventService>();

--- Together.Api/Program.cs
+++ Together.Api/Program.cs
@@ -1,7 +1,10 @@
+using Together.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IEventService, EventService>();
 }
 
 var app = builder.Build();
